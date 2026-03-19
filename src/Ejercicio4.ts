78792f20a7d2879d5d32dbb188a0430265077b35
IsviCastillo <isvijosecastillocruz@outlook.com>
2026-03-19 10:04:49 -0600

    Ejercicios

--- src/Ejercicio4.ts
+++ src/Ejercicio4.ts
@@ -2,16 +2,21 @@ abstract class Empleado {
     nombre: string;
     horasTrabajadas: number;
     salarioBase: number;
+    ventasRealizadas: number;
 
-    constructor(nombre: string, horasTrabajadas: number, salarioBase: number) {
+    constructor(nombre: string, horasTrabajadas: number, salarioBase: number ,ventasRealizadas: number = 0) {
         this.nombre = nombre;
         this.horasTrabajadas = horasTrabajadas;
         this.salarioBase = salarioBase;
+        this.ventasRealizadas = ventasRealizadas;
     }
     abstract calcularSalario(): void;
 }
 
 class EmpleadoPorHoras extends Empleado {
+    constructor(nombre: string, horasTrabajadas: number, salarioBase: number) {
+        super(nombre, horasTrabajadas, salarioBase);
+    }
     calcularSalario(): void {
         const salario = this.horasTrabajadas * this.salarioBase;
         console.log("El salario del empleado por horas " + this.nombre + " es: " + salario);
@@ -26,8 +31,21 @@ class EmpleadoFijo extends Empleado {
         console.log("El salario del empleado fijo " + this.nombre + " es: " + this.salarioBase);
     }
 }    
+class EmpleadoComision extends Empleado {
+    ventasRealizadas: number;
+    constructor(nombre: string, horasTrabajadas: number, salarioBase: number, ventasRealizadas: number) {
+        super(nombre, horasTrabajadas, salarioBase);
+        this.ventasRealizadas = ventasRealizadas;
+    }
+    calcularSalario(): void {
+        const salario = this.salarioBase + (this.ventasRealizadas * 0.1);
+        console.log("El salario del empleado por comisión " + this.nombre + " es: " + salario);
+    }
+}
 
 const empleadoHoras = new EmpleadoPorHoras("Elvis", 40, 15);
 empleadoHoras.calcularSalario();
 const empleadoFijo = new EmpleadoFijo("Dania", 0, 2000);
 empleadoFijo.calcularSalario();
+const empleadoComision = new EmpleadoComision("John", 0, 1000, 5000);
+empleadoComision.calcularSalario();
--- src/Ejercicio5.ts
+++ src/Ejercicio5.ts
@@ -1,26 +1,41 @@
 abstract class Pago {
-    abstract procesarPago(): void;
+    total: string;
+    constructor(total: string) {
+        this.total = total;
+    }
+    abstract procesarPago(a: string): void;
 }
 class PagoEfectivo extends Pago {
-    procesarPago(): void {
-        console.log("Procesando pago en efectivo");
+    procesarPago(efectivo: string): void {
+        let exec= parseFloat(efectivo)-parseFloat(this.total);
+        console.log("Cambio: " + exec);
     }
 }
 class PagoTarjeta extends Pago {
-    procesarPago(): void {
-        console.log("Procesando pago con tarjeta");
+    
+    procesarPago(efectivo: string): void {
+        if(parseFloat(efectivo) >= parseFloat(this.total)){
+            let exec= parseFloat(efectivo)-parseFloat(this.total);
+            let nuevoSaldo= exec;
+            console.log("Saldo restante: " + nuevoSaldo);
+        }
+        else{
+            console.log("Targeta declinada");
+        }
     }
-}
+    }
+
+
 class TransferenciaBancaria extends Pago {
-    procesarPago(): void {
+    procesarPago(saldo: string): void {
         console.log("Procesando transferencia bancaria");
     }
 }
-const pagoEfectivo = new PagoEfectivo();
-pagoEfectivo.procesarPago();
+const pagoEfectivo = new PagoEfectivo("25");
+pagoEfectivo.procesarPago("50");
 
-const pagoTarjeta = new PagoTarjeta();
-pagoTarjeta.procesarPago();
+const pagoTarjeta = new PagoTarjeta("100");
+pagoTarjeta.procesarPago("100");
 
-const transferenciaBancaria = new TransferenciaBancaria();
-transferenciaBancaria.procesarPago();
+const transferenciaBancaria = new TransferenciaBancaria("200");
+transferenciaBancaria.procesarPago("200");
